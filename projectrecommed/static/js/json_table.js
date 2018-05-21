ecac5297ce3543e3981b0fc29be0ecda3ac3760e
Aavash Khatri <aavashkhatri95@gmail.com>
2018-05-21 14:26:58 +0545

    Design added json-table

--- projectrecommed/static/js/json_table.js
+++ projectrecommed/static/js/json_table.js
@@ -31,14 +31,17 @@ endpoint = 'http://localhost:8000/api/recommend/'+ b
 function createTable(column, row){
 
 	var inner_container = document.createElement("div"), th, td;
+	var head = document.createElement("thead");
     var th = document.createElement("tr");
     for (var i = 0; i < column.length; i++) {
         header_tag = document.createElement("th");
                 text = document.createTextNode(column[i]);
                 header_tag.appendChild(text);
                 th.appendChild(header_tag);
-        inner_container.appendChild(th);
     }
+    inner_container.appendChild(th);
+    inner_container.appendChild(head);
+
     document.getElementById("json-table").appendChild( inner_container );
 
 
