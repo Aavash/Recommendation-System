0242d5d32821216bfae347c94edb31cbb1cdc827
Aavash Khatri <aavashkhatri95@gmail.com>
2018-05-21 10:42:53 +0545

    Datable add

--- projectrecommed/static/projectrecommend/js/json_table.js
+++ projectrecommed/static/projectrecommend/js/json_table.js
@@ -1,9 +1,31 @@
 window.onload= function () {
+console.log('good')
 
-$(document).ready(function() {
-    var datatable = $('#smsInfo').DataTable({
-    });
+    $(document).ready( function () {
+    var endpoint = "{% url 'recommend-api'%}";
+    var datatable = $('#myTable').DataTable();
+
+    $.ajax({
+              method: 'GET',
+                url: endpoint,
+                success: function(data){
+                console.log(data)
+                datatable.Columns.Add("CustLName", typeof(String));
+
+                },
+                error: function(error_data){
+                    console.log("error");
+                    datatable.Columns.Add("CustLName", typeof(String));
+
+                }
+
+            })
 
 
-    }
-  }
+
+} );
+}
+
+
+
+
