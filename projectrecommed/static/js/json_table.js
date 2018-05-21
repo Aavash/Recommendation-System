ecc0d95628909a790ac7b09318b9939fee1380a4
Aavash Khatri <aavashkhatri95@gmail.com>
2018-05-21 13:24:54 +0545

    json table added

--- projectrecommed/static/js/json_table.js
+++ projectrecommed/static/js/json_table.js
@@ -1,9 +1,58 @@
 window.onload= function () {
+var a=window.location.href
+var b= a.split('http://localhost:8000/json-table/').join('')
+endpoint = 'http://localhost:8000/api/recommend/'+ b
 
-$(document).ready(function() {
-    var datatable = $('#smsInfo').DataTable({
-    });
+    $(document).ready( function () {
+//    var datatable = $('#json-table').DataTable();
 
 
+   $.ajax({
+     method: "GET",
+     url:  endpoint ,
+     data: $("#registerSubmit").serialize(),
+     dataType: 'json',
+     success: function(data){
+     column = data.keys
+     row = data.values
+
+     createTable(column, row)
+
+
+     }
+   })
+
+
+
+} );
+}
+
+
+function createTable(column, row){
+
+	var inner_container = document.createElement("div"), th, td;
+    var th = document.createElement("tr");
+    for (var i = 0; i < column.length; i++) {
+        header_tag = document.createElement("th");
+                text = document.createTextNode(column[i]);
+                header_tag.appendChild(text);
+                th.appendChild(header_tag);
+        inner_container.appendChild(th);
     }
-  }
+    document.getElementById("json-table").appendChild( inner_container );
+
+
+    for (var j = 0; j < row.length; j++) {
+                tr = document.createElement("tr");
+        for (var i = 0; i < row[j].length; i++) {
+            data_tag = document.createElement("td");
+                text = document.createTextNode(row[j][i]);
+                data_tag.appendChild(text);
+                tr.appendChild(data_tag);
+        inner_container.appendChild(tr);
+    }
+}
+ document.getElementById("json-table").appendChild( inner_container );
+
+
+}
